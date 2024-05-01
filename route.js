f3020270942f3d6a75a04d7ddb9c8788f5d3b2ff
dothethang <thangdtph39565@fpt.edu.vn>
2024-05-01 19:42:38 +0700

    fix3

--- route.js
+++ route.js
@@ -4,47 +4,47 @@ app.config(function ($routeProvider) {
         .when(
             "/home",
             {
-                templateUrl: "./views/index.html"
+                templateUrl: "index.html"
             })
         .when(
             "/home/m1s1",
             {
-                templateUrl: "./views/m1s1.html"
+                templateUrl: "m1s1.html"
             })
         .when(
             "/home/m1s2",
             {
-                templateUrl: "./views/m1s2.html"
+                templateUrl: "m1s2.html"
             })
         .when(
             "/home/m1s3",
             {
-                templateUrl: "./views/m1s3.html"
+                templateUrl: "m1s3.html"
             })
         .when(
             "/home/m1s4",
             {
-                templateUrl: "./views/m1s4.html"
+                templateUrl: "m1s4.html"
             })
         .when(
             "/home/m1s5",
             {
-                templateUrl: "./views/m1s5.html"
+                templateUrl: "m1s5.html"
             })
         .when(
             "/home/m1s6",
             {
-                templateUrl: "./views/m1s6.html"
+                templateUrl: "m1s6.html"
             })
         .when(
             "/home/m1s7",
             {
-                templateUrl: "./views/m1s7.html"
+                templateUrl: "m1s7.html"
             })
         .when(
             "/home/page_cal_tool",
             {
-                templateUrl: "./views/page_cal_tool.html"
+                templateUrl: "page_cal_tool.html"
             })
 
         .otherwise({
